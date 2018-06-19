5a07e65fba916f0cd0a3ee5c688eb93bcb08b8be
Babubhai <shubhsharma10@gmail.com>
2018-06-19 02:48:13 -0400

    Finished album page

--- src/app/album-page/album-page.component.ts
+++ src/app/album-page/album-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {MusixMatchAPIServiceClient} from '../services/musixmatch.service.client';
 
 @Component({
   selector: 'app-album-page',
@@ -7,8 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumPageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute,
+              private musicService: MusixMatchAPIServiceClient) {
+    this.route.params.subscribe(params => this.setParams(params));
+  }
+  tracks = [];
+  artistId = 0;
+  artistName: string;
+  albumId = 0;
+  albumName: string;
+  trackCount = 0;
+  trackReleaseYear: string;
+  albumCoverArt: any;
+  setParams(params) {
+    if (params['albumId']) {
+      this.albumId = params['albumId'];
+      this.loadAlbum(this.albumId);
+      this.loadAlbumTracks(this.albumId);
+    }
+  }
+  loadAlbum(albumId) {
+    this.musicService
+      .getAlbum(albumId)
+      .then((result) => {
+        const album = result.message.body.album;
+          this.artistName = album.artist_name;
+          this.artistId = album.artist_id;
+          this.albumName = album.album_name;
+          this.trackCount = album.album_track_count;
+          this.trackReleaseYear = album.album_release_date.split('-')[0];
+          this.albumCoverArt = album.album_coverart_100x100;
+      });
+  }
+  loadAlbumTracks(albumId) {
+    this.musicService
+      .getAlbumTracks(albumId)
+      .then((result) => {
+        const trackList = (result as any).message.body.track_list;
+        this.tracks = trackList.map(x => x.track);
+      });
+  }
+  getTrackLength(trackLength) {
+    let trackLen = trackLength;
+    trackLen = trackLen / 60;
+    return trackLen.toFixed(2);
+  }
   ngOnInit() {
   }
 
