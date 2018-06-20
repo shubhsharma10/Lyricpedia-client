6932f23bd5e9d7bafc8881d4232287318a268bf1
Babubhai <shubhsharma10@gmail.com>
2018-06-20 00:48:41 -0400

    Point to heroku server

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,17 +1,15 @@
 export const API_KEY = '629bf02832ff5f4513c42b46453c2340';
 
 export const USER_API_URL =
-  'http://localhost:4000/api/user';
+  'https://lyricpedia-server.herokuapp.com/api/user';
 export const PROFILE_API_URL =
-  'http://localhost:4000/api/profile';
+  'https://lyricpedia-server.herokuapp.com/api/profile';
 export const SESSION_API_URL =
-  'http://localhost:4000/api/session';
+  'https://lyricpedia-server.herokuapp.com/api/session';
 export const LOGIN_API_URL =
-  'http://localhost:4000/api/login';
+  'https://lyricpedia-server.herokuapp.com/api/login';
 export const LOGOUT_API_URL =
-  'http://localhost:4000/api/logout';
+  'https://lyricpedia-server.herokuapp.com/api/logout';
 
 export const SONG_API_URL =
-  'http://localhost:4000/api/song';
-export const FAV_API_URL =
-  'http://localhost:4000/api/song/SID/fav';
+  'https://lyricpedia-server.herokuapp.com/api/song';
