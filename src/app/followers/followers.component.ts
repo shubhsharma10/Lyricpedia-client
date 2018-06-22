7d9e11e2fb8af77511efa9caa552c16de8e5ed5d
Babubhai <shubhsharma10@gmail.com>
2018-06-22 07:58:59 -0400

    Followers and Following implemented

--- src/app/followers/followers.component.ts
+++ src/app/followers/followers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/user.model.client';
+import {UserServiceClient} from '../services/user.service.client';
 
 @Component({
   selector: 'app-followers',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./followers.component.css']
 })
 export class FollowersComponent implements OnInit {
-
-  constructor() { }
-
+  currentUser: User = new User();
+  userNotLoggedIn = true;
+  constructor(private userService: UserServiceClient) { }
   ngOnInit() {
+    this.userService
+      .isUserLoggedIn()
+      .then((result) => {
+        if (result.status === 200) {
+          this.userNotLoggedIn = false;
+          return this.userService.profile();
+        } else {
+          this.userNotLoggedIn = true;
+          throw new Error('No user logged in');
+        }
+      })
+      .then((result) => {
+        this.currentUser = result as User;
+      })
+      .catch(() => {
+        this.userNotLoggedIn = true;
+      });
   }
 
 }
--- src/app/following/following.component.ts
+++ src/app/following/following.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserServiceClient} from '../services/user.service.client';
+import {User} from '../models/user.model.client';
 
 @Component({
   selector: 'app-following',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./following.component.css']
 })
 export class FollowingComponent implements OnInit {
-
-  constructor() { }
-
+  userNotLoggedIn = true;
+  currentUser: User = new User();
+  constructor(private userService: UserServiceClient) { }
   ngOnInit() {
+    this.userService
+      .isUserLoggedIn()
+      .then((result) => {
+        if (result.status === 200) {
+          this.userNotLoggedIn = false;
+          return this.userService.profile();
+        } else {
+          this.userNotLoggedIn = true;
+          throw new Error('No user logged in');
+        }
+      })
+      .then((result) => {
+        this.currentUser = result as User;
+      })
+      .catch(() => {
+        this.userNotLoggedIn = true;
+      });
   }
 
 }
